f52910a063dbaa1a104301b5f6f06945d58ce91e
JONATHAS GABRIEL DAVIDE TENORIO <2009641@aluno.univesp.br>
2021-10-28 20:07:19 -0300

    inicio do desenvolvimento

--- frontend-pi-um/src/pages/login/Login.tsx
+++ frontend-pi-um/src/pages/login/Login.tsx
@@ -20,7 +20,7 @@ export default function Login(){
   <WrapperPage>
     <Grid container item xs={12}>
       <Typography>
-        <p>Faça aqui seu login!</p>
+        <p>Faça aqui o seu login!</p>
       </Typography>
     </Grid>
     <Box sx={{ height: 501}} >
@@ -39,9 +39,9 @@ export default function Login(){
                 </Box>
               </Grid>
               <Grid container direction="column" justifyContent="center" alignItems="stretch" mb={2}>
-                <Button type="submit" text="Login"  />
+                <Button type="submit" text="Login" />
               </Grid>
-              <Grid container justifyContent="center" alignItems="center" direction="row" columns={2} mb={2} spacing={11}>
+              <Grid container justifyContent="center" alignItems="center" direction="row" columns={2} mb={2} spacing={7}>
                 <Grid item>
                   <Link className={styles.link} to="#">Esqueceu a senha?</Link>
                 </Grid>
